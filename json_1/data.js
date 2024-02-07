c4fd15b5f3ded323a5ec5257af822304eda71bae
Abdul Wahab Amir <83786802+A-Wahab-Aamir@users.noreply.github.com>
2024-02-07 10:16:29 +0500

    PROJECT_1

---
+++ json_1/data.js
@@ -0,0 +1,20 @@
+var data=[
+    {
+        id:101,
+        name:"abdul wahab amir",
+        profile:"img/1.jpg",
+        class:"BSSE"
+    }, 
+    {
+        id:102,
+        name:"bilal ali",
+        profile:"img/2.jpg",
+        class:"BSCS"
+    }, 
+    {
+        id:103,
+        name:"farhan khan",
+        profile:"img/3.jpg",
+        class:"MS-CSIT"
+    }
+]
