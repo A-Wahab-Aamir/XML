09a37991210c3172f8a7680ab9bb2859295ac9c8
Abdul Wahab Amir <83786802+A-Wahab-Aamir@users.noreply.github.com>
2024-02-07 10:17:16 +0500

    PROJECT_2

---
+++ json_2/student.js
@@ -0,0 +1,58 @@
+var data=[
+    {
+        id:0, 
+        "Name":"Sana Ali",
+        "f_Name":"Yaqoob Ali",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"img/1.jpg"
+    },
+    {
+        id:1, 
+        "Name":"Yasir",
+        "f_Name":"Mahmood",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"img/2.jpg"
+    },
+    {
+        id:2, 
+        "Name":"Ali Khan",
+        "f_Name":"Tameez khan",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"img/3.jpg"
+    },
+    {
+        id:3, 
+        "Name":"Maria Khan",
+        "f_Name":"Bilal Khan",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"img/4.jpg"
+    },
+    {
+        id:4, 
+        "Name":"Farooq Hadiyat",
+        "f_Name":"Hadiayat",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"img/7.jpg"
+    },
+    {
+        id:5, 
+        "Name":"Maroof Ali",
+        "f_Name":"khan Ali",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"img/5.jpg"
+    },
+    {
+        id:6, 
+        "Name":"Khan zada",
+        "f_Name":"khan Ali",
+        "class":"ViB",
+        "contact":"0311-823674",
+        "profile":"img/6.jpg"
+    },
+]
